78c89757b29eaa46412f6740534852bc387adda1
unknown <pedro_de_sousa@outlook.com>
2022-12-16 09:51:12 +0100

    working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import '../css/app.css';
-import Home from './components/Home';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "../css/app.css";
+import TestComponent from "./test";
 
-ReactDOM.render(<Router><Home /></Router>, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <TestComponent />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
